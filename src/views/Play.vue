066f4fe5ff93da08aacedc4091d1224130160a3a
dipadana <putudipadana@gmail.com>
2019-10-18 13:13:24 +0700

    update function finish

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -9,7 +9,11 @@
 <script>
 import { db, increment } from '../api/firebase'
 import { access } from 'fs'
+const firebase = require('firebase/app')
+// require('firebase/firestore')
+const dbtest = db.collection('tesuji');
 const dbtestplay = db.collection('testplay');
+const wpmdata = db.collection('wpmdata')
 
 export default {
   methods: {
@@ -19,7 +23,22 @@ export default {
     },
     finishPlay(){
       console.log("Permainan selesai")
-      dbtestplay.doc('bvHKUAFUhEW3RjzHsyTd').update
+      dbtestplay.doc('bvHKUAFUhEW3RjzHsyTd').update({play:false})
+      .then(() => {
+        return dbtest.doc('fXrCkZeVk1MzCvd7Dg2A').update({ counter: 0 })
+      })
+      .then(() => {
+        return wpmdata.doc('kR4Iem0Ow2SLhz1GWqBm').update({
+          wpm:firebase.firestore.FieldValue.arrayUnion(8) // Masukkan angka dengan jumla
+        })  
+      })
+      .then(()=>{
+        console.log("update selesai")
+      })
+      .catch(err => {
+        console.log("data")
+        console.log(err)
+      })
     }
   }
 }
