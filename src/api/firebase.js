24738cc2dbfaf45a3a7837e5783f8ffc7d7d3fca
Dipadana Putu <39789452+dipadana@users.noreply.github.com>
2019-10-20 17:38:30 +0700

    Update firebase.js

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -2,13 +2,7 @@ const firebase = require('firebase/app')
 require('firebase/firestore')
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCfqFnOg1AeggkuyQUC9T76zjl2S4aPuSI',
-  authDomain: 'balapanngetik.firebaseapp.com',
-  databaseURL: 'https://balapanngetik.firebaseio.com',
-  projectId: 'balapanngetik',
-  storageBucket: 'balapanngetik.appspot.com',
-  messagingSenderId: '127225675511',
-  appId: '1:127225675511:web:b19e36aeffa54eee7d9df2'
+  
 }
 
 firebase.initializeApp(firebaseConfig)
